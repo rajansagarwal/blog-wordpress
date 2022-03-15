8a32a8c85e1f06e0e299905821a9a0cc3b920647
Rajan Agarwal <64426829+rajanwastaken@users.noreply.github.com>
2022-03-14 22:52:29 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -15,7 +15,7 @@ export default function Index({ allPosts: { edges }, preview }) {
     <>
       <Layout preview={preview}>
         <Head>
-          <title>Next.js Blog Example with {CMS_NAME}</title>
+          <title>{CMS_NAME} - Blog</title>
         </Head>
         <Container>
           <Intro />
